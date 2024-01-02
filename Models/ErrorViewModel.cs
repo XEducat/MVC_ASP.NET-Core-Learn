5f1a0b93a363b5e4ac1aac507aa3fc82eb4c475f
XEducat <sichevskiy2004@gmail.com>
2024-01-02 23:51:53 +0200

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_ASP.NET_Core_Learn.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
