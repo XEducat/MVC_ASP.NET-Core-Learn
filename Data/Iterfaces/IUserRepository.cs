f71f3f99be415d05f0b40034da4c75230dee5b23
XEducat <sichevskiy2004@gmail.com>
2024-02-18 19:23:38 +0200

    Add User controller adn Repository

---
+++ Data/Iterfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MVC_ASP.NET_Core_Learn.Models;
+
+namespace MVC_ASP.NET_Core_Learn.Data.Iterfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        //Task<User> GetByIdAsync(int Id);
+
+        bool Add(User deposit);
+        bool Update(User deposit);
+        bool Delete(User deposit);
+        bool Save();
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_ASP.NET_Core_Learn.Data;
+using MVC_ASP.NET_Core_Learn.Data.Iterfaces;
+using MVC_ASP.NET_Core_Learn.Models;
+
+namespace MVC_ASP.NET_Core_Learn.Repository
+{
+	public class UserRepository : IUserRepository
+	{
+		private readonly AppDbContext _context;
+
+		public UserRepository(AppDbContext appDbContext)
+        {
+			_context = appDbContext;
+		}
+
+        public bool Add(User user)
+        {
+            _context.Add(user);
+            return Save();
+        }
+
+        public bool Delete(User user)
+        {
+            _context.Remove(user);
+            return Save();
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        //public async Task<AppUser> GetByIdAsync(int Id)
+        //{
+        //    return await _context.Users.FirstOrDefaultAsync(u => u.Id == Id);
+        //}
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(User user)
+        {
+            _context.Update(user);
+            return Save();
+        }
+    }
+}
