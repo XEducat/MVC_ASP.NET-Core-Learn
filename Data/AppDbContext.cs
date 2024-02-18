49ef2bfd09da23c2cd3ecb795ca3aeb60a2f3766
XEducat <sichevskiy2004@gmail.com>
2024-02-18 20:53:27 +0200

    Connect EF Core Identity

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MVC_ASP.NET_Core_Learn.Models;
 
 namespace MVC_ASP.NET_Core_Learn.Data
 {
-	public class AppDbContext : DbContext
+	public class AppDbContext : IdentityDbContext<AppUser>
 	{
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
--- Program.cs
+++ Program.cs
@@ -1,30 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVC_ASP.NET_Core_Learn.Data;
 using MVC_ASP.NET_Core_Learn.Data.Iterfaces;
+using MVC_ASP.NET_Core_Learn.Models;
 using MVC_ASP.NET_Core_Learn.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Add services to the dependency container for working with repositories.
 builder.Services.AddScoped<IDepositRepository, DepositRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-// Configure the application's DbContext to use SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+	.AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie();
 
 var app = builder.Build();
 
 // Filling the database with testing data
-using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-    var context = scope.ServiceProvider.GetService<AppDbContext>();
-    Seed.Initialize(context);
+	await Seed.SeedUsersAndRolesAsync(app);
+	//Seed.Initialize(app);
 }
 
 // Configure the HTTP request pipeline.
