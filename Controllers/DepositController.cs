0aa08bd11176dbfd741c0c9bd4fa0019896970c5
XEducat <sichevskiy2004@gmail.com>
2024-02-20 01:55:12 +0200

    Added edit logic to Deposit
    Added:
    - View для зміни депозитів.
    - ViewModel яка представляє слой з валідацією для зручної взаємодії View з моделькою.
    - Частково налаштовані стилі для View змініи депозиту.

--- Controllers/DepositController.cs
+++ Controllers/DepositController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_ASP.NET_Core_Learn.Data.Iterfaces;
 using MVC_ASP.NET_Core_Learn.Models;
+using MVC_ASP.NET_Core_Learn.ViewModels;
 
 namespace MVC_ASP.NET_Core_Learn.Controllers
 {
@@ -32,5 +33,58 @@ namespace MVC_ASP.NET_Core_Learn.Controllers
 		{
 			return View();
 		}
-	}
+
+		public async Task<IActionResult> Edit(int id)
+		{
+			var deposit = await _depositRepository.GetByIdAsync(id);
+			if(deposit == null) return View("Error");
+			var depositVM = new EditDepositViewModel()
+			{
+				Id = id,
+				Title = deposit.Title,
+				ShortDescription = deposit.ShortDescription,
+				Replenishment = deposit.Replenishment,
+				InterestRate = deposit.InterestRate,
+				Term = deposit.Terms,
+                InterestRateEarlyClosure = deposit.InterestRateEarlyClosure,
+				InterestRateNoEarlyClosure = deposit.InterestRateNoEarlyClosure,
+			};
+
+			return View(depositVM);
+		}
+
+		[HttpPost]
+        public async Task<IActionResult> Edit(int id, EditDepositViewModel depositVM)
+		{
+			if (!ModelState.IsValid)
+			{
+                ModelState.AddModelError("", "Failed to edit deposit");
+				return View("Edit", depositVM);	
+			}
+
+			var userDeposit = await _depositRepository.GetByIdAsyncNoTraking(id);
+
+			if (userDeposit != null)
+			{
+				var deposit = new Deposit()
+				{
+					Id = id,
+					Title = depositVM.Title,
+					ShortDescription = depositVM.ShortDescription,
+					Replenishment = depositVM.Replenishment,
+					InterestRate = depositVM.InterestRate,
+					Terms = depositVM.Term,
+					InterestRateEarlyClosure = depositVM.InterestRateEarlyClosure,
+					InterestRateNoEarlyClosure = depositVM.InterestRateNoEarlyClosure,
+				};
+
+				_depositRepository.Update(deposit);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(depositVM);
+			}
+		}
+    }
 }
---
+++ ViewModels/EditDepositViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MVC_ASP.NET_Core_Learn.Data.Enums;
+using MVC_ASP.NET_Core_Learn.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_ASP.NET_Core_Learn.ViewModels
+{
+	public class EditDepositViewModel
+	{
+        public int Id { get; set; }
+		[Display(Name = "Назва депозиту")]
+		public string Title { get; set; }
+
+		[Display(Name = "Короткий опис депозиту")]
+		public string ShortDescription { get; set; }
+
+		[Display(Name = "Поповнення можливе")]
+		public bool Replenishment { get; set; }              
+
+        [Display(Name = "Виплата процентів")]
+        public InterestRate InterestRate { get; set; }
+
+        [Display(Name = "Терміни депозиту")]
+        public IEnumerable<DepositTerm> Term { get; set; }   // Термін депозиту, [не доступний для зміни адміном наразі]
+
+        [Display(Name = "Ставка без дострокового закриття(Вводити числа, виключно через символ '.')")]
+        public double? InterestRateNoEarlyClosure { get; set; }
+
+        [Display(Name = "Ставка з достроковим закриттям(Вводити числа, виключно через символ '.')")]
+        public double? InterestRateEarlyClosure { get; set; }   
+	}
+}
