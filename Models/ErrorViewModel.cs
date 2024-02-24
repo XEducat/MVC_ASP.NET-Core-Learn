8dc06a2eca20a7fd35873573d79ecd73b4770492
XEducat <sichevskiy2004@gmail.com>
2024-02-25 01:55:09 +0200

    Error view can show Model.Errors

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,5 +5,7 @@ namespace MVC_ASP.NET_Core_Learn.Models
 		public string? RequestId { get; set; }
 
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+		public List<string> Errors { get; set; }
 	}
 }
