c2f491e991757e704ce10546a9a0792bd40bc768
Vadim <113421299+XEducat@users.noreply.github.com>
2024-03-12 20:42:21 +0200

    Extension to Enums Description
    Add:
    - Новий метод Enum для отримання значення змінної в коректному текстовому вигяді.
    
    Edit:
    - Виведення Enum тепер відбувається через розширений метод GetDescription
    
    Delete:
    - Видалено метод GetInterestRateInString класу Deposit т.я. розширення цілковито його замінює.

---
+++ Data/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace MVC_ASP.NET_Core_Learn.Data.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static string GetDescription(this Enum value)
+		{
+			// Отримуємо тип перерахування
+			Type type = value.GetType();
+
+			// Отримуємо член перерахування за значенням
+			MemberInfo[] memInfo = type.GetMember(value.ToString());
+
+			if (memInfo.Length > 0)
+			{
+				// Отримуємо атрибут Description, якщо він існує
+				object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+				if (attrs.Length > 0)
+				{
+					// Повертаємо опис з атрибута Description
+					return ((DescriptionAttribute)attrs[0]).Description;
+				}
+			}
+
+			// Якщо атрибут Description не знайдено, просто повертаємо рядок перерахування
+			return value.ToString();
+		}
+	}
+}
--- Models/Deposit.cs
+++ Models/Deposit.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using MVC_ASP.NET_Core_Learn.Data.Enums;
-using System.ComponentModel;
 
 
 namespace MVC_ASP.NET_Core_Learn.Models
@@ -11,18 +10,11 @@ namespace MVC_ASP.NET_Core_Learn.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }                          // Айді
         public string Title { get; set; }                    // Назва депозиту
-        public string ShortDescription { get; set; }         // Короткий опис депозиту--------------------------------------------------------------------
+        public string ShortDescription { get; set; }         // Короткий опис депозиту
         public bool Replenishment { get; set; }              // Поповнення
         public InterestPayment InterestPayment { get; set; }       // Виплата процентів
         public List<DepositTerm> Terms { get; set; }   // Термін депозиту
         public double? InterestRateNoEarlyClosure { get; set; } // Ставка без дострокового закриття
         public double? InterestRateEarlyClosure { get; set; }   // Ставка з достроковим закриттям
-
-        public string GetInterestRateInString()
-		{
-			var field = InterestPayment.GetType().GetField(InterestPayment.ToString());
-			var descriptionAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
-			return descriptionAttribute?.Description ?? InterestPayment.ToString();
-		}
 	}
 }
