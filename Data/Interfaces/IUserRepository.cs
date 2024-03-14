f1388cda7721cd7fd923aa230921dba33f1bc143
Vadim <113421299+XEducat@users.noreply.github.com>
2024-03-14 21:03:37 +0200

    Deleted unused UserRepository
    Edit:
    - Відключено UserRepository.
    
    Deleted:
    - Видалено UserRepository та відповідний до нього інтерфейс.

--- Data/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MVC_ASP.NET_Core_Learn.Models;
-
-namespace MVC_ASP.NET_Core_Learn.Data.Interfaces
-{
-	public interface IUserRepository : IBaseRepository<AppUser>
-	{
-		Task<AppUser> GetUserById(string Id);
-	}
-}
