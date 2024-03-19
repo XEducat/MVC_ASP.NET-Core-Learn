0fff87989d0d522cbb160990eb0c3d884c9094cc
Vadim <113421299+XEducat@users.noreply.github.com>
2024-03-19 22:31:09 +0200

    Added replenishment function
    Added:
    - Додано сторінку для поповнення депозиту.
    - Додано логіку для поповнення депозиту.

--- Controllers/UserDepositController.cs
+++ Controllers/UserDepositController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MVC_ASP.NET_Core_Learn.Data;
 using MVC_ASP.NET_Core_Learn.Data.Interfaces;
 using MVC_ASP.NET_Core_Learn.Models;
-using MVC_ASP.NET_Core_Learn.Repository;
 using MVC_ASP.NET_Core_Learn.ViewModels;
 
 namespace MVC_ASP.NET_Core_Learn.Controllers
@@ -94,6 +91,33 @@ namespace MVC_ASP.NET_Core_Learn.Controllers
             IEnumerable<UserDeposit> userDeposits = await _userDepositRepository.GetDepositsByUserIdAsync(currentUser.Id);
 
 			return View(userDeposits);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReplenishmentDeposit(int id)
+        {
+            var selectedUserDeposit = await _userDepositRepository.GetByIdAsync(id);
+
+            // Передача необхідних данних депозиту в ViewBag
+            ViewBag.UserDepositId = id; 
+            ViewBag.SelectedDepositTitle = selectedUserDeposit.Title;
+            return View();
+        }
+
+        // TODO: Можливо варто додати дату останнього поповнення та винести дані в ViewModel
+        [HttpPost]
+        public async Task<IActionResult> ReplenishmentDeposit(decimal replenishmentAmount, int Id)
+        {
+            if (replenishmentAmount <= 0) return RedirectToAction("ReplenishmentDeposit");
+
+            var selectedUserDeposit = await _userDepositRepository.GetByIdAsync(Id);
+
+            // Оновлення суму депозиту користувача, додаючи введену суму поповнення
+            selectedUserDeposit.Amount += replenishmentAmount;
+            _userDepositRepository.Update(selectedUserDeposit);
+
+            // Перенаправте користувача на іншу сторінку або відобразіть повідомлення про успішне поповнення
+            return RedirectToAction("All");
         }
     }
 }
