1193720e6eea0dcb94fde5017be9a4b8b7b1db7e
Vadim <113421299+XEducat@users.noreply.github.com>
2024-03-21 00:09:05 +0200

    Added the ability to create new templates for the deposit
    Added:
    - Додано та винесено в окремий файл, скрипти які дозволяють додавати та видаляти терімі з шаблону депозита.
    
    Edit:
    - Представлення відкореговано для правильного відображення та передачі.
    - Представлення перероблено в більшій мірі під Html хелпери.

--- Models/DepositTerm.cs
+++ Models/DepositTerm.cs
@@ -2,6 +2,7 @@
 {
     public class DepositTerm
     {
+        // TODO: Можливо переробити вз'язок таблиці "Один термін -> Декілька шаблонів"
         public int Id { get; set; }
         public int NumberMonths { get; set; }
     }
---
+++ wwwroot/js/term-interactions.js
@@ -0,0 +1,33 @@
+﻿var addButton = document.getElementById("add-term-button");
+var removeButton = document.getElementById("remove-term-button");
+var termListContainer = document.getElementById("term-list-container");
+var inputTemplate = document.getElementById("input-template");
+
+addButton.addEventListener("click", addInput);
+removeButton.addEventListener("click", removeInput);
+
+function addInput() {
+    var newIndex = termListContainer.children.length; // Получаем текущее количество элементов
+    var inputClone = inputTemplate.content.cloneNode(true);
+    inputClone.querySelector('.term-input').name = `terms[${newIndex}].NumberMonths`; // Присваиваем правильный индекс
+    termListContainer.appendChild(inputClone);
+}
+
+function removeInput() {
+    var lastTermItem = termListContainer.lastElementChild;
+    if (lastTermItem) {
+        lastTermItem.remove();
+    }
+}
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    var termInputs = document.querySelectorAll('.term-input');
+    var terms = [];
+
+    termInputs.forEach(function (input) {
+        var termValue = input.value.trim();
+        if (termValue !== "") {
+            terms.push({ NumberMonths: termValue });
+        }
+    });
+});
